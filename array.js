e552e2f97e97e634df40f12ca465f289f28c4f77
riduuu <faridhossainseoprovider01@gmail.com>
2024-10-08 20:59:26 +0300

    push

---
+++ array.js
@@ -0,0 +1,17 @@
+
+let todoList = ['Do Laundry', 'Pay Bil', 'Walk the dog'];
+todoList.push('Buy groceries')
+console.log(todoList);
+
+todoList.unshift('Clean room');
+console.log(todoList);
+
+todoList.pop()
+console.log(todoList);
+
+console.log(todoList.indexOf('Clean room'));
+
+todoList.shift();
+console.log(todoList);
+
+console.log(todoList.length);
