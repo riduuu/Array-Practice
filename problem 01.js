2238a3b3c9b38f1c6fb029739b0de52cdd2d1d2c
riduuu <faridhossainseoprovider01@gmail.com>
2024-11-24 20:33:29 +0600

    push

--- problem 01.js
+++ problem 01.js
@@ -1,18 +1,22 @@
-
+// step 01
 let todoList = ['Do Laundry', 'Pay Bil', 'Walk the dog'];
 todoList.push('Buy groceries')
 console.log(todoList);
 
+// step 02
 todoList.unshift('Clean room');
 console.log(todoList);
 
+// step 03
 todoList.pop()
 let storeInVariable = [todoList]
 console.log(todoList);
 
+// step 04
 console.log(todoList.indexOf('Clean room'));
 
 todoList.shift();
 console.log(todoList);
 
+// step 05
 console.log(todoList.length);
---
+++ problem 02.js
@@ -0,0 +1,4 @@
+// step 01
+let guestList = ['Alice', 'Bob', 'john doe']
+guestList.push('john doe', 'tom', 'jerry')
+console.log(guestList)
